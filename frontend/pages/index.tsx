df07dfb570a603e7937bd6d427d47c44edcf92a0
Arnel <167875243+Arnel-rah@users.noreply.github.com>
2025-09-24 18:48:04 +0300

    feat(frontend): add simple homepage

---
+++ frontend/pages/index.tsx
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <div style={{ padding: 20 }}>
+      <h1>Spring Boot + Next.js Starter Kit</h1>
+      <p>Frontend is working </p>
+    </div>
+  );
+}
